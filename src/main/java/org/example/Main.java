c68590e194ccdc22c1b7fc4154b721140cfcd80c
zakariaayl <ayougilzaki@example.com>
2024-12-13 01:19:48 +0100

    making the main class as a thread ,code need to be clean

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.sql.SQLException;
 import java.util.Date;
 
-public class Main {
+public class Main extends Thread {
     public static void DeleteFile(String path){
             File myObj = new File(path);
             if (myObj.delete()) {
@@ -34,8 +31,8 @@ public class Main {
             e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) throws IOException, SQLException {
+    @Override
+    public void run() {
         File folder =new File("src/main/java/org/example/input");
 
         if (folder!=null && folder.isDirectory()) {
@@ -49,6 +46,10 @@ public class Main {
                         while ((line =b.readLine())!=null) {
                             json.append(line.trim());
                         }
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
                     }
 
                     System.out.println("File Number: "+numfile);
@@ -66,7 +67,14 @@ public class Main {
                         String item =object.get("item").getAsString();
                         String addresse =object.get("shippingAddress").getAsString();
                         Order order=new Order(date,new customer(customerName),item,addresse,status,price);
-                        boolean b=order.saveOrder();
+                        boolean b= false;
+                        try {
+                            b = order.saveOrder();
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
                         if(b){
                             System.out.println("nice");
                             String PATH="src/main/java/org/example/output/"+item;
@@ -76,13 +84,21 @@ public class Main {
                             String PATH="src/main/java/org/example/error/"+item;
                             CreateFle(PATH);
                         }
-                        String pathtodlete="src/main/java/org/example/output/"+file.getName();
-                        DeleteFile(pathtodlete);
+
                     }
                 }
+                file.delete();
             }
         } else {
             System.out.println("walo");
         }
+
     }
-}
+    public static void main(String[] args) throws InterruptedException {
+        Main thread=new Main();
+        thread.sleep(3600*1000);
+        thread.start();
+    }
+    }
+
+
